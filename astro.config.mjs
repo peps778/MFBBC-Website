950992d56339d05a09ca81b14f121b74ced8d098
Paul Jhon Magbanua <magbanuatx56@gmail.com>
2025-06-18 02:49:52 +0800

    gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+
+  site: 'https://peps778.github.io', // your GitHub Pages URL
+  base: '/MFBBC-Website/', // must match your repo name
 });
